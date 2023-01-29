48433b689b92182a34d04e1dc5a0482f47a69040
likelasttime <a50070863@gmail.com>
2023-01-29 18:02:24 +0900

    Feat(follower-service) : SecurityConfig 추가

---
+++ follow-service/src/main/java/com/sgdevcamp/followservice/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.sgdevcamp.followservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic().disable()
+                .cors().and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .authorizeRequests()
+                        .anyRequest().permitAll()
+                .and()
+                .build();
+    }
+}
