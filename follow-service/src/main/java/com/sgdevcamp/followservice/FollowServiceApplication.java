ca28037440ca21fc8a526bd5b35bd1f8b748b5ec
likelasttime <a50070863@gmail.com>
2023-01-29 18:21:33 +0900

    Feat(follow-service) : FollowServiceApplication 수정

--- follow-service/src/main/java/com/sgdevcamp/followservice/FollowServiceApplication.java
+++ follow-service/src/main/java/com/sgdevcamp/followservice/FollowServiceApplication.java
@@ -2,8 +2,10 @@ package com.sgdevcamp.followservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 @SpringBootApplication
+@EnableNeo4jRepositories
 public class FollowServiceApplication {
 
 	public static void main(String[] args) {
