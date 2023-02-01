206c05a832937d26316684d7d259d4fb351cf9b3
likelasttime <a50070863@gmail.com>
2023-02-01 21:23:27 +0900

    Feat(post-service) : 예외 처리 추가

---
+++ post-service/src/main/java/com/sgdevcamp/postservice/exception/CustomExceptionStatus.java
@@ -0,0 +1,20 @@
+package com.sgdevcamp.postservice.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum CustomExceptionStatus {
+    SUCCESS(true, 1000, "요청에 성공하였습니다."),
+
+    NOT_ALLOWED_USER(false, 1001, "허용된 작성자가 아닙니다."),
+
+    NOT_FOUND_POST(false, 1100, "요청한 게시글을 찾을 수 없습니다."),
+    NOT_FOUND_COMMENT(false, 1101, "요청한 댓글을 찾을 수 없습니다.");
+
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+}
