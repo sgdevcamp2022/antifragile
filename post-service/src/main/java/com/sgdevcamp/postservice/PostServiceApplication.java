3ff438ef1882044e4d10177b4f0c6f4ad6fd8a41
likelasttime <a50070863@gmail.com>
2023-02-01 23:05:37 +0900

    Feat(post-service) : PostServiceApplication 수정
    MongoRepository, MongoAuditing을 사용하기 위한 어노테이션 추가

--- post-service/src/main/java/com/sgdevcamp/postservice/PostServiceApplication.java
+++ post-service/src/main/java/com/sgdevcamp/postservice/PostServiceApplication.java
@@ -2,8 +2,12 @@ package com.sgdevcamp.postservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoAuditing
 @SpringBootApplication
+@EnableMongoRepositories
 public class PostServiceApplication {
 
 	public static void main(String[] args) {
