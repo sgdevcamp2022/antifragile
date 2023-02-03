e7c900663e9b08221406310d1ccda80fef2c0ce3
likelasttime <a50070863@gmail.com>
2023-02-03 15:51:30 +0900

    Feat(membership-service) : 프로필 DTO 수정 및 추가
    인스타그램에서는 사용자 이름, 이메일도 수정할 수 있음
    우리는 사용자 이름, 이메일은 수정할 수 없게 함
    이 점은 나중에 변경될 수 있음
    전화번호는 필드로 추가하지 않아도 될 것 같지만 성별은 생각해봐야겠음

--- membership-service/src/main/java/com/sgdevcamp/membershipservice/dto/request/ProfileRequest.java
+++ membership-service/src/main/java/com/sgdevcamp/membershipservice/dto/request/ProfileRequest.java
@@ -5,10 +5,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProfileRequest {
+    @NotBlank
+    @Length(min=2, max=15)
+    @Pattern(regexp = "^[가-힣a-zA-Z0-9_-]{2,20}$")
+    private String name;
+
     @Length(max= 190)
     private String introduction;
 }
