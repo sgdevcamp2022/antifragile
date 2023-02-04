cd9b452f936ee1b7d442ccdd4d8a1deeb91a9066
likelasttime <a50070863@gmail.com>
2023-02-04 12:25:01 +0900

    Feat(follow-service) : CustomExceptionStatus 수정

--- follow-service/src/main/java/com/sgdevcamp/followservice/exception/CustomExceptionStatus.java
+++ follow-service/src/main/java/com/sgdevcamp/followservice/exception/CustomExceptionStatus.java
@@ -6,8 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum CustomExceptionStatus {
-    USERNAMEALREADYEXISTSEXCEPTION(false, 6001, "이미 존재하는 유저입니다."),
-    USERNAMENOTEXISTSEXCEPTION(false, 6002, "존재하지 않는 유저입니다.");
+    USERNAME_ALREADY_EXIST(false, 6001, "이미 존재하는 유저입니다."),
+    USERNAME_NOT_EXIST(false, 6002, "존재하지 않는 유저입니다.");
 
     private final boolean isSuccess;
     private final int code;
