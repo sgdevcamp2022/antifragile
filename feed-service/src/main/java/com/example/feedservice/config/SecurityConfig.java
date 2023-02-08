216d289f48d4438b9ba8be7ded5f2455e886a6ed
likelasttime <a50070863@gmail.com>
2023-02-08 19:48:20 +0900

    Feat(feed-service) : SecurityConfig 추가

---
+++ feed-service/src/main/java/com/example/feedservice/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.feedservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic().disable()
+                .cors().and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                .and()
+                    .authorizeRequests()
+                        .anyRequest().permitAll()
+                .and()
+                .build();
+
+    }
+
+}
