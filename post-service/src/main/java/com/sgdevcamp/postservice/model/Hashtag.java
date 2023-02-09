4039541dff2a0db3edfe9a81bc3885f13766a465
likelasttime <a50070863@gmail.com>
2023-02-09 16:20:18 +0900

    Feat(post-service) : Hashtag 도큐먼트 추가
    Post와 HashTag는 Linked되어 있음
    해시 태그를 이용해서 검색을 편하게 하기 위해 분리함

---
+++ post-service/src/main/java/com/sgdevcamp/postservice/model/Hashtag.java
@@ -0,0 +1,30 @@
+package com.sgdevcamp.postservice.model;
+
+import java.time.Instant;
+import java.util.List;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@Document
+public class Hashtag {
+    @Id
+    private String id;
+
+    private String tag;
+
+    @DocumentReference
+    private List<Post> posts;
+
+    private Long count;
+
+    @CreatedDate
+    private Instant createdAt;
+}
