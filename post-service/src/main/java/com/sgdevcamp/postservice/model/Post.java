0bff5afffdb06bc7c10b7cf52a93518655f76445
likelasttime <a50070863@gmail.com>
2023-02-09 21:05:59 +0900

    Feat(post-service) : Post 도큐먼트 hashTags 필드 수정
    상호 참조를 할 필요가 없다고 판단함
    Post에서 hashTag를 embedded하고 삐르게 조회

--- post-service/src/main/java/com/sgdevcamp/postservice/model/Post.java
+++ post-service/src/main/java/com/sgdevcamp/postservice/model/Post.java
@@ -8,7 +8,6 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.time.Instant;
 import java.util.List;
@@ -33,8 +32,7 @@ public class Post {
 
     private Long likeCount;
 
-    @DocumentReference
-    private List<Hashtag> hashTags;
+    private List<String> hashTags;
 
     @CreatedDate
     private Instant createdAt;
