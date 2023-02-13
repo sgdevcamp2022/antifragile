3611ad0066d9ff2c10abd68f759012e3c8f0bedd
likelasttime <a50070863@gmail.com>
2023-02-13 10:17:57 +0900

    Feat(post-service) : PostLike 도큐먼트 수정
    고유 식별자 id를 추가해서 좋아요 취소를 쉽게 구현할 수 있음
    이전에는 postId와 username으로 postLike 도큐먼트를 찾아서 삭제하려 했음

--- post-service/src/main/java/com/sgdevcamp/postservice/model/PostLike.java
+++ post-service/src/main/java/com/sgdevcamp/postservice/model/PostLike.java
@@ -3,12 +3,16 @@ package com.sgdevcamp.postservice.model;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Builder
 @Document
 public class PostLike {
+    @Id
+    private String id;
+
     private String postId;
 
     @CreatedBy
