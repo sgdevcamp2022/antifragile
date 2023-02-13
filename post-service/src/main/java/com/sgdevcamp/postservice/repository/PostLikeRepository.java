281b933113201cad845c1f09d8ea17273b9eb3ad
likelasttime <a50070863@gmail.com>
2023-02-13 10:54:53 +0900

    Feat(post-service) : PostLike의 service 추가 및 Repository 수정

--- post-service/src/main/java/com/sgdevcamp/postservice/repository/PostLikeRepository.java
+++ post-service/src/main/java/com/sgdevcamp/postservice/repository/PostLikeRepository.java
@@ -3,10 +3,13 @@ package com.sgdevcamp.postservice.repository;
 import com.sgdevcamp.postservice.model.PostLike;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface PostLikeRepository extends MongoRepository<PostLike, String> {
-    void deleteByPostId(String post_id);
+import java.util.List;
 
-    void deleteByPostIdAndUsername(String post_id, String username);
+public interface PostLikeRepository extends MongoRepository<PostLike, String> {
+
+    List<PostLike> findByPostId(String post_id);
+
+    void deleteAllInBatchByPostId(String post_id);
 
     Long countByPostId(String post_id);
 
