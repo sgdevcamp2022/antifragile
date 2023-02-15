359bd6e0ad42db3cc7e14942c742be69d888a22b
likelasttime <a50070863@gmail.com>
2023-02-15 15:58:36 +0900

    Test(post-service) : HashtagServiceTest에 검색 테스트 추가

--- post-service/src/test/java/com/sgdevcamp/postservice/HashtagServiceTest.java
+++ post-service/src/test/java/com/sgdevcamp/postservice/HashtagServiceTest.java
@@ -1,6 +1,8 @@
 package com.sgdevcamp.postservice;
 
 import com.sgdevcamp.postservice.model.Hashtag;
+import com.sgdevcamp.postservice.model.Image;
+import com.sgdevcamp.postservice.model.Post;
 import com.sgdevcamp.postservice.repository.HashtagRepository;
 import com.sgdevcamp.postservice.service.HashtagService;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,8 +12,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 
-import java.util.Optional;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -30,8 +34,15 @@ public class HashtagServiceTest {
 
     @BeforeEach
     public void beforeEach(){
+        Post post = Post.builder()
+                .id("1")
+                .images(Arrays.asList(new Image()))
+                .content("좋아요 눌러줘")
+                .build();
+
         hashtag = Hashtag.builder()
                 .tag("#맞팔")
+                .postId(post)
                 .build();
     }
 
@@ -46,6 +57,21 @@ public class HashtagServiceTest {
 
         // then
         assertThat(hashtag).isEqualTo(hashtag);
+    }
+
+    @Test
+    @DisplayName("해시태그 검색")
+    public void searchHashtag() {
+        // given
+        List<Hashtag> hashtagList = new ArrayList<>();
+        hashtagList.add(hashtag);
+
+        Page<Hashtag> hashtags = new PageImpl<>(hashtagList);
+
+        when(hashtagRepository.findByTagContaining(any(), any())).thenReturn(hashtags);
+
+        // when, then
+        assertThat(hashtagService.searchHashtag(any(), any())).isNotEmpty();
     }
 
 }
