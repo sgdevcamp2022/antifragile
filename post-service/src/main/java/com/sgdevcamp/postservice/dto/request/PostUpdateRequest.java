70e28225de12737268907a9cc3d948169324c1e4
likelasttime <a50070863@gmail.com>
2023-02-18 18:20:45 +0900

    Feat(post-service) : PostUpdateRequest 수정
    PostService에 있어야 할 코드를 잘못 넣어서 수정

--- post-service/src/main/java/com/sgdevcamp/postservice/dto/request/PostUpdateRequest.java
+++ post-service/src/main/java/com/sgdevcamp/postservice/dto/request/PostUpdateRequest.java
@@ -1,12 +1,9 @@
 package com.sgdevcamp.postservice.dto.request;
 
-import com.sgdevcamp.postservice.dto.response.PostResponse;
-import com.sgdevcamp.postservice.model.Post;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -16,10 +13,3 @@ public class PostUpdateRequest {
 
     private List<String> hashTags;
 }
-    public List<PostResponse> postsByUserIdIn(List<String> ids){
-
-        return postRepository.findByUserIdInOrderByCreatedAtDesc(ids)
-                .stream()
-                .map(Post::toResponse)
-                .collect(Collectors.toList());
-    }
