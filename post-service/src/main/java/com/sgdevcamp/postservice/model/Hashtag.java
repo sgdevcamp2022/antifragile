fb2b543809bda52500ec3e5a4479f2e2aa639308
likelasttime <a50070863@gmail.com>
2023-02-25 23:05:23 +0900

    Feat(post-service) : Post, Hashtag 도큐먼트에 인덱스 추가
    Post 도큐먼트의 createdAt 필드에 인덱스를 추가하니 속도 향상
    반면, Hashtag 도큐먼트는 전과 후가 차이가 크지 않았음

--- post-service/src/main/java/com/sgdevcamp/postservice/model/Hashtag.java
+++ post-service/src/main/java/com/sgdevcamp/postservice/model/Hashtag.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
@@ -17,6 +18,7 @@ public class Hashtag {
     @Id
     private String id;
 
+    @Indexed
     private String tag;
 
     @DocumentReference
